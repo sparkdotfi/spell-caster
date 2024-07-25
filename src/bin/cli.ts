d65d818a7c0deae2fc852a9de5674c3acf6c2453
Krzysztof Kaczor <krzkaczor@gmail.com>
2024-07-25 15:48:24 +0200

    Improve CLI

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,12 +1,27 @@
 import assert from 'node:assert'
+import { parseArgs } from 'node:util'
 import { forkAndExecuteSpell } from '..'
 import { getConfig } from '../config'
 import { getRequiredShellEnv } from '../config/environments/cli'
+import { ensureAbsolutePath } from '../utils/fs'
 
-async function main(spellName?: string) {
+async function main() {
+  const args = parseArgs({
+    options: {
+      root: {
+        type: 'string',
+      },
+    },
+    allowPositionals: true,
+    strict: true,
+  })
+  const rootPath = args.values.root
+  const spellName = args.positionals[0]
+
   assert(spellName, 'Pass spell name as an argument ex. SparkEthereum_20240627')
+  assert(rootPath, 'Pass root path as an argument ex. --root /path/to/spark-spells')
 
-  const config = getConfig(getRequiredShellEnv)
+  const config = getConfig(getRequiredShellEnv, ensureAbsolutePath(rootPath))
 
   console.log(`Executing spell ${spellName}`)
   const { forkRpc, appUrl } = await forkAndExecuteSpell(spellName, config)
@@ -15,5 +30,4 @@ async function main(spellName?: string) {
   console.log(`Spark App URL: ${appUrl}`)
 }
 
-const arg1 = process.argv[2]
-await main(arg1)
+await main()
