a2656e347001082db7457d50c0b5f9df1180932a
teddy <teddy@defi.sucks>
2024-12-13 12:12:55 -0300

    fix: (foundry breaking change) create requires a  --broadcast flag now

--- src/periphery/forge/index.ts
+++ src/periphery/forge/index.ts
@@ -8,7 +8,9 @@ export async function deployContract({
   from,
   cwd,
 }: { contractName: string; rpc: string; from: Address; cwd: string }): Promise<Address> {
-  const result = await $`forge create --rpc-url ${rpc} --from ${from} ${contractName} --unlocked --json`.cwd(cwd).json()
+  const result = await $`forge create  --broadcast --rpc-url ${rpc} --from ${from} ${contractName} --unlocked --json`
+    .cwd(cwd)
+    .json()
   core.info(`Deployed spell ${contractName} to address ${result.deployedTo}`)
   core.debug(`Deploy result: ${result}`)
   return result.deployedTo
